4e00cb0f14b09dfd69ad10f62cab9836310659c1
famturiop <vorobyev.seva.rules@gmail.com>
2014-07-11 01:38:44 +0400

    autopilot

---
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AirportClassLibrary1;
+using metClassLibrary2;
+
+
+
+
+
+
+namespace обучалка
+{
+    public static class airportrefit : airport
+    {
+        int lenght = 0;
+        public int Getlenght()
+        {
+            return lenght;
+        }
+        public int Setlenght(int c)
+        {
+            this.lenght = c;
+            return lenght;
+        }
+    
+    }
+
+    public class newmet : met
+    {
+        static override void center()
+        {
+            
+        }
+        public int Getlenght()
+        {
+            
+        }
+        public int Setlenght(int c)
+        {
+
+        }
+    }
+    
+    
+    class Program  //базовый класс
+    {
+        static void Main(string[] args) //точка входа в программу
+        {
+            metClassLibrary2.met.center();   //просто передача управления в наш метод-интерфейс
+            Console.WriteLine("Остановка прогр.");
+            Console.ReadKey();
+        }
+    }
+}
